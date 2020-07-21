cd418ea99480a4fc7dda48e4c8db55ce58ff0231
Yuishun <shun0ego@gmail.com>
2020-07-21 17:59:00 +0900

    ちょっと追加

---
+++ Assets/WorkSpace/Yui/Script/Rigid_Collider_enable.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rigid_Collider_enable : MonoBehaviour
+{
+    /* 自分の全ての子供のRigidbodyとColliderを操作 */
+
+    [SerializeField]
+    bool active;
+
+    List<Rigidbody> rigids = new List<Rigidbody>(); //子供のRigidbody List
+    List<Collider> cols = new List<Collider>();     // 子供のCollider List
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+        Collider[] colliders= GetComponentsInChildren<Collider>();
+
+        foreach(Rigidbody rb in rigidbodies)
+        {
+            if(rb.transform == transform)
+            {
+                rb.isKinematic = active;
+                continue;
+            }
+
+            rb.isKinematic = !active;
+            rigids.Add(rb);
+        }
+        foreach(Collider col in colliders)
+        {
+            if (col.transform == transform)
+            {
+                col.enabled = !active;
+                continue;
+            }
+
+            col.enabled = active;
+            cols.Add(col);
+        }
+    }
+
+}
