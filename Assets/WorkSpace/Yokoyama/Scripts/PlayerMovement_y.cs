608c116356f6a2d33bc9ebfd5ed6789ee65dac14
a-yokoyama <airtextcard@gmail.com>
2020-07-30 13:24:16 +0900

    y

--- Assets/WorkSpace/Yokoyama/Scripts/PlayerMovement_y.cs
+++ Assets/WorkSpace/Yokoyama/Scripts/PlayerMovement_y.cs
@@ -5,8 +5,15 @@ using static InputManager_y;
 
 public class PlayerMovement_y : MonoBehaviour
 {
-    private float VELOCITY = 10.0f;
+    [SerializeField] private float VELOCITY = 2.0f;
+    [SerializeField] private float JUMP_VELO = 5f;
+    [SerializeField] private float GRAVITY = -9.8f;
+    [SerializeField] private Rigidbody rb = null;
 
+    const float RayLength = 0.675f;
+
+    private bool bRagdolled = false;
+    private bool bGround = true;
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +24,52 @@ public class PlayerMovement_y : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(IMIsButtonOn(IM_BUTTON.RIGHT))
+        if(!bRagdolled)
         {
-            transform.position += Vector3.right * VELOCITY * Time.deltaTime;
+            NormalUpdate();
         }
-        else if(IMIsButtonOn(IM_BUTTON.LEFT))
+    }
+
+    private void FixedUpdate()
+    {
+        if(bGround)
         {
-            transform.position -= Vector3.right * VELOCITY * Time.deltaTime;
+            if(IMIsButtonOn(IM_BUTTON.JUMP))
+            {
+                Jump();
+            }
         }
+        else
+        {
+            Ray ray_ = new Ray(transform.position, Vector3.down);
+            RaycastHit hitInfo_ = new RaycastHit();
+
+            if(Physics.Raycast(ray_, RayLength))
+            {
+                bGround = true;
+            }
+        }
+    }
+
+
+    //  非ラグドール時の移動処理
+    private void NormalUpdate()
+    {
+        Vector3 vecDelta_ = Vector3.zero;
+
+        //  左右移動
+        float horAxis_ = IMGetAxisValue(IM_AXIS.L_STICK_X);
+        vecDelta_ += horAxis_ * Vector3.right * VELOCITY * Time.deltaTime;
+
+        transform.position += vecDelta_;
+
+        Debug.DrawLine(transform.position, transform.position + Vector3.down * RayLength);
+    }
+
+    //  ジャンプ処理(fixedUpdate)
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * JUMP_VELO, ForceMode.Impulse);
+        bGround = false;
     }
 }
