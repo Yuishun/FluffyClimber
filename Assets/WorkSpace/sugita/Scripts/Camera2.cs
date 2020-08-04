12f8bea71c30bfd955040da8aea7be7d66fd7fbc
sugita-tadayosi <0jc2p.1b1r3165j@ez.web.ne.jp>
2020-08-04 13:22:25 +0900

    a

--- Assets/WorkSpace/sugita/Scripts/Camera2.cs
+++ Assets/WorkSpace/sugita/Scripts/Camera2.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Camera2 : MonoBehaviour
 {
-    GameObject _player;
+    GameObject _player,_wall;
 
     // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.Find("Sphere");
+        _wall = GameObject.Find("Cube");
 
         transform.position =
             new Vector3(transform.position.x,
@@ -25,20 +26,20 @@ public class Camera2 : MonoBehaviour
     void Update()
     {
 
-        if (_player.transform.position.y >= 10 && flag2 == false)
+        if (_player.transform.position.x >= -6.5 && _player.transform.position.y >= 10 && flag2 == false)
         {
             transform.position =
             new Vector3(transform.position.x,
-            _player.transform.position.y + 5,
+            _player.transform.position.y + 4,
             transform.position.z);
             flag2 = true;
         }
 
-        else if (_player.transform.position.y >= 0 && flag == false)
+        else if (_player.transform.position.x >= 3.5 && _player.transform.position.y >= 0 && flag == false)
         {
             transform.position =
             new Vector3(transform.position.x,
-            _player.transform.position.y + 5,
+            _player.transform.position.y + 4,
             transform.position.z);
             flag = true;
         }
