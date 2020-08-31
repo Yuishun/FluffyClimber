7b8e8d2f4b87d176f0022b03e0acaf9f1a95829d
sugita-tadayosi <0jc2p.1b1r3165j@ez.web.ne.jp>
2020-08-31 18:05:07 +0900

    カメラ移動

--- Assets/WorkSpace/sugita/Scripts/Camera2.cs
+++ Assets/WorkSpace/sugita/Scripts/Camera2.cs
@@ -6,27 +6,16 @@ public class Camera2 : MonoBehaviour
 {
     GameObject _player;
 
-    bool flag;
-    bool flag2;
 
-    [SerializeField]
-    private Sou _up;
-    [SerializeField]
-    private Sou2 _up2;
-  
+    public List<Sou> upArea = new List<Sou>();
+    public List<float> cameraY = new List<float>();
+
+    
     // Start is called before the first frame update
     void Start()
     {
         //_player = GameObject.Find("Sphere");
         _player = GameObject.Find("hito_model");
-
-        flag = true;
-        flag2 = true;
-
-        transform.position =
-            new Vector3(transform.position.x,
-            transform.position.y - 6,
-            transform.position.z);
 
     }
 
@@ -34,22 +23,15 @@ public class Camera2 : MonoBehaviour
     void Update()
     {
 
-        if (_up.UpCamera()== flag)
+        if (upArea[0].UpCamera())
         {
             transform.position =
             new Vector3(transform.position.x,
-            _player.transform.position.y + 4,
+            cameraY[0],
             transform.position.z);
-            flag = false;
-        }
 
-        if (_up2.UpCamera() == flag2)
-        {
-            transform.position =
-            new Vector3(transform.position.x,
-            _player.transform.position.y + 4,
-            transform.position.z);
-            flag2 = false;
+            upArea.RemoveAt(0);
+            cameraY.RemoveAt(0);
         }
     }
 }
