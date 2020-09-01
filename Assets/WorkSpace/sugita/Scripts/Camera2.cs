1964bb250f2c98b7d0b36f9e696a6126df23f957
Yuishun <shun0ego@gmail.com>
2020-09-01 10:51:47 +0900

    カメラ修正

--- Assets/WorkSpace/sugita/Scripts/Camera2.cs
+++ Assets/WorkSpace/sugita/Scripts/Camera2.cs
@@ -5,42 +5,67 @@ using UnityEngine.Events;
 
 public class Camera2 : MonoBehaviour
 {
-    GameObject _player;
+    [SerializeField]
+    Transform _player;
 
 
     public List<Sou> upArea = new List<Sou>();
     public List<float> cameraY = new List<float>();
 
-    
+    int Index = 1;
+
+    Rect rect = new Rect(0, 0, 1, 1); // 画面内か判定するためのRect
+
     // Start is called before the first frame update
     void Start()
     {
         //_player = GameObject.Find("Sphere");
-        _player = GameObject.Find("hito_model");
+        //_player = GameObject.Find("hito_model").transform.GetChild(0);
 
         UnityAction act_ = this.ChangeBGM;
 
         AudioManager.StopBGM(true, 0.5f, act_);
+
+        upArea.Insert(0, new Sou());
+        cameraY.Insert(0, transform.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
+        var viewPos=Camera.main.WorldToViewportPoint(_player.position);
+        if (!rect.Contains(viewPos) &&
+            _player.position.y < transform.position.y)
+        {
+            upArea[--Index].Flag = false;
+            Index--;
+            if (Index < 0)
+                Index = 0;
+            transform.position =
+                        new Vector3(transform.position.x,
+                        cameraY[Index],
+                        transform.position.z);            
 
-        if (upArea[0].UpCamera())
+            if (Index == 0)
+                Index = 1;
+        }
+
+
+        if (upArea[Index].UpCamera())
         {
             transform.position =
             new Vector3(transform.position.x,
-            cameraY[0],
+            cameraY[Index],
             transform.position.z);
 
-            upArea.RemoveAt(0);
-            cameraY.RemoveAt(0);
+            Index++;
         }
+        
+
     }
 
     private void ChangeBGM()
     {
-        AudioManager.PlayBGM(AudioManager.BGM.game);
+        AudioManager.PlayBGM(AudioManager.BGM.game, 0.25f);
     }
 }
--- Assets/WorkSpace/sugita/Scripts/Sou.cs
+++ Assets/WorkSpace/sugita/Scripts/Sou.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Sou : MonoBehaviour
 {
     bool m_flag;
+    public bool Flag{set{ m_flag = value; }}
 
     // Start is called before the first frame update
     void Start()
