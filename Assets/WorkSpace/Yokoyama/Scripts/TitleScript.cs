ee2d5222f029ec951cf53a4c0010e43c689783fc
a-yokoyama <airtextcard@gmail.com>
2020-09-01 09:27:43 +0900

    y

--- Assets/WorkSpace/Yokoyama/Scripts/TitleScript.cs
+++ Assets/WorkSpace/Yokoyama/Scripts/TitleScript.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class TitleScript : MonoBehaviour
 {
+    [SerializeField] private GameObject hito;
+
+    private float timer = 0;
+    GameObject prevObj = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +29,24 @@ public class TitleScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if(timer >= 3f)
+        {
+            if (prevObj)
+                Destroy(prevObj);
+
+            timer = 0;
+
+            GameObject obj_ = Instantiate(hito, new Vector3(Random.value * 0.5f - 1f, 2, 1), Quaternion.identity) as GameObject;
+            if(obj_)
+            {
+                Rigidbody rb_ = obj_.GetComponent<Rigidbody>();
+                if(rb_)
+                {
+                    rb_.AddTorque(new Vector3(Random.value, Random.value, Random.value));
+                }
+            }
+        }
     }
 }
