f552ceb5d246195ca8fd361a03ebccb0e07b286c
a-yokoyama <airtextcard@gmail.com>
2020-10-06 14:34:27 +0900

    ステージセレクト

--- Assets/WorkSpace/Yokoyama/Scripts/AnimationButton.cs
+++ Assets/WorkSpace/Yokoyama/Scripts/AnimationButton.cs
@@ -3,8 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartButton : MonoBehaviour
+public class AnimationButton : MonoBehaviour
 {
+    [SerializeField] private string LoadSceneName = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
     public void Clicked()
     {
         AudioManager.PlaySE(AudioManager.SE.button);
@@ -15,13 +29,13 @@ public class StartButton : MonoBehaviour
     {
         float timer = 0f;
 
-        while(timer < 1.2f)
+        while (timer < 1.2f)
         {
             timer += Time.deltaTime;
             yield return 0;
         }
 
-        SceneManager.LoadScene("StageSelect");
+        SceneManager.LoadScene(LoadSceneName);
         yield break;
     }
 }
