797e6941bead5521e480a1e9b386e2c75615a80c
sugita-tadayosi <0jc2p.1b1r3165j@ez.web.ne.jp>
2020-12-16 13:13:53 +0900

    即死壁の色変化

---
+++ Assets/WorkSpace/sugita/Scripts/change.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class change : MonoBehaviour
+{
+     Material _material;
+
+     GameObject _wall;
+
+    float r, g, b, a, up;
+
+    bool c_flag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _wall = GameObject.Find("Wall");
+        _material = _wall.GetComponent<Renderer>().material;
+        r = 0;
+        g = 0;
+        b = 0;
+        a = 0;
+        up = 0;
+        c_flag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(up)
+        {
+            case 0:
+                r += Time.deltaTime;
+                if(r > 1)
+                    up = 1;
+                break;
+
+            case 1:
+                r -= Time.deltaTime;
+                g += Time.deltaTime;
+                if(g > 1 && r < 0)
+                    up = 2;
+                break;
+
+            case 2:
+                g -= Time.deltaTime;
+                b += Time.deltaTime;
+                if(b > 1 && g < 0)
+                    up = 3;
+                break;
+
+            case 3:
+                r += Time.deltaTime;
+                b -= Time.deltaTime;
+                if (r > 1)
+                    up = 1;
+                break;
+        }
+        _material.color = new Color(r, g, b);
+    }
+}
