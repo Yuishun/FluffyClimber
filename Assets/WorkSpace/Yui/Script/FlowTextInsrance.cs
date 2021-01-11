0b7cf2b9082808e3289ffc634c3087a29f1cd3ec
Yuishun <shun0ego@gmail.com>
2021-01-11 22:32:07 +0900

    StageEXの修正。

---
+++ Assets/WorkSpace/Yui/Script/FlowTextInsrance.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowTextInsrance : MonoBehaviour
+{
+    public void FlowingText(string str)
+    {
+        FlowTextManager.instance.FlowingText(str);
+    }
+
+}
