a71a1613a89d9096da0ab3b94734571589309b70
Yuishun <shun0ego@gmail.com>
2021-01-12 14:50:23 +0900

    修正

--- Assets/WorkSpace/Yui/Script/FlowTextInsrance.cs
+++ Assets/WorkSpace/Yui/Script/FlowTextInsrance.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class FlowTextInsrance : MonoBehaviour
 {
+    private void Start()
+    {
+        Vibration.instance.StopVibration();
+    }
+
     public void FlowingText(string str)
     {
         FlowTextManager.instance.FlowingText(str);
